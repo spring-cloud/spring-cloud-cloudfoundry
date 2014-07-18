1a0b7e71a520367fc68c004a24862c7e5d702131
Dave Syer <dsyer@gopivotal.com>
2014-07-18 17:50:31 +0100

    Remove jersey dependency

--- src/test/java/org/springframework/platform/cloudfoundry/broker/sample/Application.java
+++ src/test/java/org/springframework/platform/cloudfoundry/broker/sample/Application.java
@@ -17,30 +17,14 @@ package org.springframework.platform.cloudfoundry.broker.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 @Configuration
 @EnableAutoConfiguration
-public class Application extends WebMvcConfigurerAdapter {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public FilterRegistrationBean jersey() {
-		FilterRegistrationBean bean = new FilterRegistrationBean();
-		bean.setFilter(new ServletContainer());
-		bean.addInitParameter("com.sun.jersey.config.property.WebPageContentRegex",
-				"(/|/(flex/|images/|js/|css/|jsp/|admin/|v2/catalog|v2/service_instances).*)");
-		bean.addInitParameter("com.sun.jersey.config.property.packages",
-				"com.sun.jersey;com.netflix");
-		return bean;
 	}
 
 }
