7f80f7616812d80f292aab089908d064158fc882
Dave Syer <dsyer@pivotal.io>
2014-08-29 09:50:33 +0100

    Make tokenInfoUri explicit

--- src/main/java/org/springframework/platform/cloudfoundry/sso/CloudfoundrySsoProperties.java
+++ src/main/java/org/springframework/platform/cloudfoundry/sso/CloudfoundrySsoProperties.java
@@ -34,6 +34,9 @@ public class CloudfoundrySsoProperties implements Validator {
 	@Value("${vcap.services.sso.credentials.tokenUri:}")
 	private String tokenUri;
 
+	@Value("${vcap.services.sso.credentials.tokenInfoUri:}")
+	private String tokenInfoUri;
+
 	@Value("${vcap.services.sso.credentials.authorizationUri:}")
 	private String authorizationUri;
 
@@ -42,17 +45,18 @@ public class CloudfoundrySsoProperties implements Validator {
 
 	@Value("${vcap.services.sso.credentials.clientSecret:}")
 	private String clientSecret;
-	
+
 	private Home home = new Home();
 
 	@Data
-	public static class Home {		
-		private String path = "/";		
+	public static class Home {
+		private String path = "/";
 		private boolean secure = true;
 	}
 
 	public String getTokenInfoUri() {
-		return tokenUri.replace("/oauth/token", "/check_token");
+		return StringUtils.hasText(tokenInfoUri) ? tokenInfoUri : tokenUri.replace(
+				"/oauth/token", "/check_token");
 	}
 
 	public String getUserInfoUri() {
@@ -60,26 +64,28 @@ public class CloudfoundrySsoProperties implements Validator {
 	}
 
 	public String getLogoutUri(String redirectUrl) {
-		return tokenUri.replace("/oauth/token", "/logout.do?redirect="+redirectUrl);
+		return tokenUri.replace("/oauth/token", "/logout.do?redirect=" + redirectUrl);
 	}
-	
+
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return CloudfoundrySsoProperties.class.isAssignableFrom(clazz);
 	}
-	
+
 	@Override
 	public void validate(Object target, Errors errors) {
 		CloudfoundrySsoProperties sso = (CloudfoundrySsoProperties) target;
 		if (StringUtils.hasText(sso.getClientId())) {
 			if (!StringUtils.hasText(sso.getAuthorizationUri())) {
-				errors.rejectValue("authorizeUri", "missing.authorizeUri", "Missing authorizeUri");
+				errors.rejectValue("authorizeUri", "missing.authorizeUri",
+						"Missing authorizeUri");
 			}
 			if (!StringUtils.hasText(sso.getTokenUri())) {
 				errors.rejectValue("tokenUri", "missing.tokenUri", "Missing tokenUri");
 			}
 			if (!StringUtils.hasText(sso.getClientSecret())) {
-				errors.rejectValue("clientSecret", "missing.clientSecret", "Missing clientSecret");
+				errors.rejectValue("clientSecret", "missing.clientSecret",
+						"Missing clientSecret");
 			}
 		}
 	}
