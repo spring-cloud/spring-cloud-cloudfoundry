4282f4aeab94fb3f7abfc996a318d9a829dd5175
Dave Syer <dsyer@pivotal.io>
2014-08-29 13:34:36 +0100

    Update package names

---
+++ src/main/java/org/springframework/cloud/cloudfoundry/resource/CloudfoundryResourceConfiguration.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.cloudfoundry.resource;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.util.ClassUtils;
+
+/**
+ * @author Dave Syer
+ *
+ */
+@Configuration
+@ConditionalOnExpression("'${cloudfoundry.resource.clientId:${vcap.services.resource.credentials.clientId:}}'!=''")
+@ConditionalOnClass({ EnableResourceServer.class, SecurityProperties.class })
+@ConditionalOnWebApplication
+@EnableResourceServer
+@EnableConfigurationProperties(CloudfoundryResourceProperties.class)
+public class CloudfoundryResourceConfiguration {
+
+	@Autowired
+	private CloudfoundryResourceProperties resource;
+	
+	@Bean
+	@ConditionalOnMissingBean(ResourceServerConfigurer.class)
+	public ResourceServerConfigurer resourceServer() {
+		return new ResourceSecurityConfigurer(resource);
+	}
+	
+	@Bean
+	@ConditionalOnMissingBean(ResourceServerTokenServices.class)
+	public ResourceServerTokenServices tokenServices() {
+		RemoteTokenServices services = new RemoteTokenServices();
+		services.setCheckTokenEndpointUrl(resource.getTokenInfoUri());
+		services.setClientId(resource.getClientId());
+		services.setClientSecret(resource.getClientSecret());
+		return services;
+	}
+
+
+
+	protected static class ResourceSecurityConfigurer extends ResourceServerConfigurerAdapter {
+
+		private CloudfoundryResourceProperties resource;
+
+		@Autowired
+		public ResourceSecurityConfigurer(CloudfoundryResourceProperties resource) {
+			this.resource = resource;
+		}
+
+		@Override
+		public void configure(ResourceServerSecurityConfigurer resources)
+				throws Exception {
+			resources.resourceId(resource.getResourceId());
+		}
+		
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().authenticated();
+		}
+
+	}
+
+	@Configuration
+	protected static class ResourceServerOrderProcessor implements BeanPostProcessor {
+
+		@Override
+		public Object postProcessAfterInitialization(Object bean, String beanName)
+				throws BeansException {
+			if (bean instanceof ResourceServerConfiguration) {
+				ResourceServerConfiguration configuration = (ResourceServerConfiguration) bean;
+				configuration.setOrder(getOrder());
+			}
+			return bean;
+		}
+
+		@Override
+		public Object postProcessBeforeInitialization(Object bean, String beanName)
+				throws BeansException {
+			return bean;
+		}
+
+		private int getOrder() {
+			if (ClassUtils
+					.isPresent(
+							"org.springframework.boot.actuate.autoconfigure.ManagementServerProperties",
+							null)) {
+				return ManagementServerProperties.ACCESS_OVERRIDE_ORDER - 10;
+			}
+			return SecurityProperties.ACCESS_OVERRIDE_ORDER - 10;
+		}
+
+	}
+	
+}
