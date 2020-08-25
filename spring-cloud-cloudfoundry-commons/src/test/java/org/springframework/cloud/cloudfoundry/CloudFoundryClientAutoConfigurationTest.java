1993675c70916b8d2dd1ceb70c8ff158da7c9d79
Mushtaq Ahmed <amushtaq@vmware.com>
2020-08-25 17:45:40 +0530

    Bump cf-java-client.version from 3.9.0.RELEASE to 4.9.0.RELEASE and fix failing tests after upgrade... Changes to fix the test case in CloudFoundryAppServiceDiscoveryClientTest.java and uncomment tests in CloudFoundryClientAutoConfigurationTest.java

--- spring-cloud-cloudfoundry-commons/src/test/java/org/springframework/cloud/cloudfoundry/CloudFoundryClientAutoConfigurationTest.java
+++ spring-cloud-cloudfoundry-commons/src/test/java/org/springframework/cloud/cloudfoundry/CloudFoundryClientAutoConfigurationTest.java
@@ -26,7 +26,6 @@ import org.cloudfoundry.reactor.tokenprovider.PasswordGrantTokenProvider;
 import org.cloudfoundry.reactor.uaa.ReactorUaaClient;
 import org.cloudfoundry.routing.RoutingClient;
 import org.junit.Assume;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
@@ -58,7 +57,7 @@ public class CloudFoundryClientAutoConfigurationTest {
 	}
 
 	@Test
-	@Ignore // see https://github.com/spring-cloud/spring-cloud-cloudfoundry/issues/65
+	// @Ignore // see https://github.com/spring-cloud/spring-cloud-cloudfoundry/issues/65
 	public void autoConfiguresBeansWithAllProperties() {
 		this.contextRunner.withPropertyValues("spring.cloud.cloudfoundry.username=user",
 				"spring.cloud.cloudfoundry.password=secret",
@@ -74,7 +73,7 @@ public class CloudFoundryClientAutoConfigurationTest {
 	}
 
 	@Test
-	@Ignore // see https://github.com/spring-cloud/spring-cloud-cloudfoundry/issues/65
+	// @Ignore // see https://github.com/spring-cloud/spring-cloud-cloudfoundry/issues/65
 	public void autoConfiguresBeansWithMinimalProperties() {
 		this.contextRunner.withPropertyValues("spring.cloud.cloudfoundry.username=user",
 				"spring.cloud.cloudfoundry.password=secret").run((context) -> {
